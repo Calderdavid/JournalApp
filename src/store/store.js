3530d228ee14fa09bbcca8710ced72ec25addaa9
David Calderon <davidjcr200040@gmail.com>
2022-06-06 00:00:05 -0400

    setting up store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { authSlice } from './slices/authSlice';
+import {legacy_createStore as createStore, combineReducers} from 'redux';
+import { authReducer } from '../reducers/authReducer';
 
-
-
-export const store = configureStore({
-    reducer: {
-        auth: authSlice.reducer
-    }
+const reducers = combineReducers({
+    auth: authReducer
 })
+
+export const store = createStore(reducers);
+
+
+
+
+
+
+
+
+
+// import { configureStore } from '@reduxjs/toolkit'
+// import { authSlice } from './slices/authSlice';
+
+
+
+// export const store = configureStore({
+//     reducer: {
+//         auth: authSlice.reducer
+//     }
+// })
+
 
